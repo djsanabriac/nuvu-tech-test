2d323ec30a33f6f2c12a1275f491cc2816d181f1
johan.sanabria <johan.sanabria@fitideas.co>
2020-12-27 10:10:14 -0500

    Creación de proyecto back
    Script de creación de base de datos y usuario

---
+++ back-nuvu-tech-test/src/main/java/co/djsanabriac/nuvutest/NuvutestApplication.java
@@ -0,0 +1,13 @@
+package co.djsanabriac.nuvutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuvutestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuvutestApplication.class, args);
+    }
+
+}
---
+++ back-nuvu-tech-test/src/test/java/co/djsanabriac/nuvutest/NuvutestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.djsanabriac.nuvutest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuvutestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
