948b9d6333f6101b6e49e2ac54100a35abbd3bff
Johan_Sanabria <djsanabriac@unal.edu.co>
2020-12-27 19:42:48 -0500

    Servicio de clientes #8
    Se crea bajo la ruta /clients

---
+++ back-nuvu-tech-test/src/main/java/co/djsanabriac/nuvutest/controller/ClientController.java
@@ -0,0 +1,31 @@
+package co.djsanabriac.nuvutest.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getClients(){
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity createClient(){
+        return ResponseEntity.ok("dummy");
+    }
+
+    @PutMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity updateClient(){
+        return ResponseEntity.ok("dummy");
+    }
+
+    @DeleteMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity deleteClient(){
+        return ResponseEntity.ok("dummy");
+    }
+
+}
