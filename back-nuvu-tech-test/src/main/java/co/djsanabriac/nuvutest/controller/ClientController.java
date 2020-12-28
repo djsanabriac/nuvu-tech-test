765ee60f96e0dc8ec4855cfb7e6eb318e53bb141
johan.sanabria <johan.sanabria@fitideas.co>
2020-12-27 20:04:29 -0500

    GET /clients #9
    El servicio retorna todos los usuarios que notienen username y contraseña asignados

--- back-nuvu-tech-test/src/main/java/co/djsanabriac/nuvutest/controller/ClientController.java
+++ back-nuvu-tech-test/src/main/java/co/djsanabriac/nuvutest/controller/ClientController.java
@@ -1,16 +1,32 @@
 package co.djsanabriac.nuvutest.controller;
 
+import co.djsanabriac.nuvutest.model.dto.GeneralResponse;
+import co.djsanabriac.nuvutest.model.entity.User;
+import co.djsanabriac.nuvutest.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity getClients(){
-        return ResponseEntity.ok("dummy");
+
+        List<User> toReturn = new ArrayList<>();
+
+        toReturn.addAll(userRepository.findAllByUsernameOrEmailIsNull(null));
+
+        return ResponseEntity.ok(new GeneralResponse(true,
+                "success", toReturn).toMap());
     }
 
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
--- back-nuvu-tech-test/src/main/java/co/djsanabriac/nuvutest/repository/UserRepository.java
+++ back-nuvu-tech-test/src/main/java/co/djsanabriac/nuvutest/repository/UserRepository.java
@@ -3,6 +3,11 @@ package co.djsanabriac.nuvutest.repository;
 import co.djsanabriac.nuvutest.model.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByUsernameOrEmail(String username, String email);
+
+    List<User> findAllByUsernameOrEmailIsNull(User user);
+
 }
